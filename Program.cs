26236b91972bf6154f8800c66287abfeb5a6dc3f
sdmkhan123 <sdmkhan126410@gmail.com>
2021-10-06 21:51:06 +0530

    Welcome to File IO Practicinng

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FileIOPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("===============================================================================\n");
+            Console.WriteLine("Welcome to File IO practice problem");
+            Console.WriteLine("\n===============================================================================\n");
+        }
+    }
+}
